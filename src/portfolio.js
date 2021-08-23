4b274e710acb399287f4c3e0d7c615fb20e0093e
mohamedbenismail <mohamedwaelbenismail@gmail.com>
2021-08-23 18:00:48 +0200

    fixing

--- src/portfolio.js
+++ src/portfolio.js
@@ -26,11 +26,9 @@ const greeting = {
 const socialMediaLinks = {
   github: "https://github.com/Hamabenismail",
   linkedin: "https://www.linkedin.com/in/benismail/",
-  gmail: "saadpasta70@gmail.com",
+  gmail: "mohamedwaelbenismail@gmail.com",
   gitlab: "https://gitlab.com/saadpasta",
-  facebook: "https://www.facebook.com/saad.pasta7",
   medium: "https://medium.com/@saadpasta",
-  stackoverflow: "https://stackoverflow.com/users/10422806/saad-pasta",
   // Instagram and Twitter are also supported in the links!
   display: true // Set true to display this section, defaults to false
 };
